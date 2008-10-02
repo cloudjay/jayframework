4e1dca316b7b88021d3bbad64fb8e716c24689f1
cloudjay <cloudjay@1c189804-901a-11dd-88a1-c356c4ec900a>
2008-10-02 01:44:25 +0000

    new project

---
+++ trunk/JayFramework.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : JayFramework.cpp
+// Author      : Jay
+// Version     :
+// Copyright   : copyleft
+// Description : Hello World in C, Ansi-style
+//============================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hello World!");
+	return EXIT_SUCCESS;
+}
