becd5bd2fa626c0c11ea7e3511c40fc27190fbdc
cloudjay <cloudjay@1c189804-901a-11dd-88a1-c356c4ec900a>
2008-10-11 10:13:20 +0000

    #ifdef UNITTEST

--- JayFramework.cpp
+++ JayFramework.cpp
@@ -1,20 +1,18 @@
-//============================================================================
-// Name        : JayFramework.cpp
-// Author      : Jay
-// Copyright   : copyleft
-//============================================================================
-
 #include <stdio.h>
 #include <stdlib.h>
 #include "JayFramework.h"
 
 int main(void) {
+#ifdef UNITTEST
 	RunTests();
+#endif
 	return EXIT_SUCCESS;
 }
 
+#ifdef UNITTEST
 int RunTests()
 {
 	puts("Unit testing...");
 	return UnitTest::RunAllTests();
 }
+#endif
