19d4910a83bd466ab3f16fa5f47d10f9047426d6
cloudjay <cloudjay@1c189804-901a-11dd-88a1-c356c4ec900a>
2008-10-14 06:38:32 +0000

    Including define.h

--- JayFramework.h
+++ JayFramework.h
@@ -1,13 +1,18 @@
 #ifndef JAYFRAMEWORK_H_
 #define JAYFRAMEWORK_H_
 
+#include "define.h"
+
+#ifdef EXPAT
+// XML Document encoding is UTF-16
+#define  XML_UNICODE_WCHAR_T
+#include "expat/expat.h"
+#endif
+
 #ifdef UNITTEST
 #include "UnitTest++/UnitTest++.h"
 
 int RunTests();
 #endif
 
-#ifdef EXPAT
-#include "expat/expat.h"
-#endif
 #endif /* JAYFRAMEWORK_H_ */
