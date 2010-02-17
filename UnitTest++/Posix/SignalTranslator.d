7231d7ffa6a3293a79bc3ae4449b71dd03a710b9
cloudjay <cloudjay@1c189804-901a-11dd-88a1-c356c4ec900a>
2010-02-17 16:02:22 +0000

    Posix UnitTest++

---
+++ UnitTest++/Posix/SignalTranslator.d
@@ -0,0 +1,28 @@
+src/Posix/SignalTranslator.o:  src/Posix/SignalTranslator.cpp \
+  src/Posix/SignalTranslator.h /usr/include/setjmp.h /usr/include/_ansi.h \
+  /usr/include/newlib.h /usr/include/sys/config.h \
+  /usr/include/machine/ieeefp.h /usr/include/cygwin/config.h \
+  /usr/include/machine/setjmp.h /usr/include/signal.h \
+  /usr/include/sys/signal.h /usr/include/_ansi.h \
+  /usr/include/sys/features.h /usr/include/sys/types.h \
+  /usr/include/_ansi.h /usr/include/machine/_types.h \
+  /usr/include/machine/_default_types.h /usr/include/sys/_types.h \
+  /usr/include/sys/lock.h \
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/stddef.h \
+  /usr/include/machine/types.h /usr/include/cygwin/types.h \
+  /usr/include/sys/sysmacros.h /usr/include/stdint.h \
+  /usr/include/endian.h /usr/include/cygwin/signal.h
+src/Posix/SignalTranslator.cpp :
+  src/Posix/SignalTranslator.h /usr/include/setjmp.h /usr/include/_ansi.h :
+  /usr/include/newlib.h /usr/include/sys/config.h :
+  /usr/include/machine/ieeefp.h /usr/include/cygwin/config.h :
+  /usr/include/machine/setjmp.h /usr/include/signal.h :
+  /usr/include/sys/signal.h /usr/include/_ansi.h :
+  /usr/include/sys/features.h /usr/include/sys/types.h :
+  /usr/include/_ansi.h /usr/include/machine/_types.h :
+  /usr/include/machine/_default_types.h /usr/include/sys/_types.h :
+  /usr/include/sys/lock.h :
+  /usr/lib/gcc/i686-pc-cygwin/3.4.4/include/stddef.h :
+  /usr/include/machine/types.h /usr/include/cygwin/types.h :
+  /usr/include/sys/sysmacros.h /usr/include/stdint.h :
+  /usr/include/endian.h /usr/include/cygwin/signal.h :
